953fc474d95ea2bfe1b710f588020f2641a46083
Marco <theovation95@gmail.com>
2024-11-12 11:19:09 +0100

    Update decode_audio.py

--- scripts/decode_audio.py
+++ scripts/decode_audio.py
@@ -17,40 +17,38 @@ for dirpath, dirnames, filenames in os.walk(main_folder):
 
 secret = """'59.183.111.96:57926/i*d*58.47.106.191:51900/bin.sh*d*117.213.92.42:56783/Mozi.m'"""
 
-print(audio_mp3)
-print(audio_wav)
-# for audio in audio_mp3[:]:
-# 	print(audio)
-# 	command = "./hideme " + audio + " -f"
-# 	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# 	process.wait()
-# 	stdout, stderr = process.communicate()
-# 	output = stdout.decode('latin-1')
-# 	match = re.search(r"Message: ('[^']*')", output)
-# 	if match:
-# 		message_value = match.group(1)
-# 		if message_value[:len(secret)] == secret:
-# 			print("Secret correctly decoded!")
-# 		else:
-# 			print("Error")
-# 	else:
-# 		print("Message field not found.")
+for audio in audio_mp3[:]:
+	print(audio)
+	command = "./hideme " + audio + " -f"
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	process.wait()
+	stdout, stderr = process.communicate()
+	output = stdout.decode('latin-1')
+	match = re.search(r"Message: ('[^']*')", output)
+	if match:
+		message_value = match.group(1)
+		if message_value[:len(secret)] == secret:
+			print("Secret correctly decoded!")
+		else:
+			print("Error")
+	else:
+		print("Message field not found.")
 
-# for audio in audio_wav[:]:
-# 	print(audio)
-# 	command = "./hideme " + audio + " -f"
-# 	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-# 	process.wait()
-# 	stdout, stderr = process.communicate()
-# 	output = stdout.decode('latin-1')
-# 	match = re.search(r"Message: ('[^']*')", output)
-# 	if match:
-# 		message_value = match.group(1)
-# 		if message_value[:len(secret)] == secret:
-# 			print("Secret correctly decoded!")
-# 		else:
-# 			print("Error")
-# 	else:
-# 		print("Message field not found.")
+for audio in audio_wav[:]:
+	print(audio)
+	command = "./hideme " + audio + " -f"
+	process = subprocess.Popen(command, shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+	process.wait()
+	stdout, stderr = process.communicate()
+	output = stdout.decode('latin-1')
+	match = re.search(r"Message: ('[^']*')", output)
+	if match:
+		message_value = match.group(1)
+		if message_value[:len(secret)] == secret:
+			print("Secret correctly decoded!")
+		else:
+			print("Error")
+	else:
+		print("Message field not found.")
 
 
